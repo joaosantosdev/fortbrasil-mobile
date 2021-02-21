f75a9b24167c322c2572e039bd23d0006ab42a0f
Your Name <you@example.com>
2021-02-21 17:18:00 -0300

    initial config

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+        <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
